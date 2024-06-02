2c9ae552d0a17430ef44a22ff4b4e7b83ae92df9
daniwalter001 <daniwalter001@gmail.com>
2024-06-02 17:42:29 +0000

    V1

---
+++ main.py
@@ -0,0 +1,350 @@
+from utils import (
+    Consts,
+    getLiveLink,
+    getToken,
+    getVodDetails,
+    handleChoice,
+    headers_g,
+    headers_token,
+    getAllStreams,
+    wait,
+    getAllGenres,
+    clear,
+    playWithMPV,
+    getAllVodGenres,
+    getAllVodbyGenres,
+    getVodLink,
+)
+from urllib.parse import urlparse
+
+# server = "http://line2.protv.cc/c/"
+server = "http://600600.org:8080/c/"
+host = "".join(server.split("/c/"))
+# mac = "00:1A:79:00:94:57"
+mac = "00:1A:79:B0:B0:B2"
+headers_token["Referer"] = server
+headers_token["Host"] = urlparse(server).netloc
+headers_token["Cookie"] = f"mac={mac.strip()}; stb_lang=en; timezone=GMT;"
+headers_g["Referer"] = server
+headers_g["Host"] = urlparse(server).netloc
+headers_g["Cookie"] = f"mac={mac.strip()}; stb_lang=en; timezone=GMT;"
+
+
+def tvChoice(token: str):
+    if not token:
+        print("Token non généré...exiting...\n")
+        return
+    genres = getAllGenres(host, headers_g, token)
+    allChannels = getAllStreams(token, host, headers_g)
+    allChannels = allChannels if allChannels else []
+
+    while True:
+        clear()
+        if not genres or len(genres) == 0:
+            print("Echec lors de la recup des groupes de chaines\n")
+            break
+
+        print("Affichage des canaux")
+        for i, el in enumerate(genres):
+            print(f"{i+1}. {genres[el]}")
+
+        print("Choix : ")
+        genreChoice = input()
+        if genreChoice == "0" or genreChoice.lower() == "q":
+            break
+        try:
+            genreChoice = int(genreChoice)
+        except ValueError:
+            print("Choix invalide\n")
+            continue
+
+        if genreChoice < 1 or genreChoice > len(genres):
+            print("Choix invalide\n")
+            continue
+
+        clear()
+
+        genreSelectedId = list(genres.keys())[genreChoice - 1]
+        genreSelected = genres[list(genres.keys())[genreChoice - 1]]
+
+        channels = list(
+            filter(lambda x: x["tv_genre_id"] == genreSelectedId, allChannels)
+        )
+
+        while True:
+            clear()
+            print(f"Affichage des canaux de {genreSelected}\r")
+            for i, el in enumerate(channels):
+                print(f"{i+1}. {el['name']}")
+
+            print("Choisis une chaine : ")
+            channelChoice = input()
+            if channelChoice == "0" or channelChoice.lower() == "q":
+                break
+            try:
+                channelChoice = int(channelChoice)
+            except ValueError:
+                print("Choix invalide\n")
+                continue
+
+            if channelChoice < 1 or channelChoice > len(channels):
+                print("Choix invalide\n")
+                continue
+
+            channelSelected = channels[channelChoice - 1]
+
+            # print(channelSelected)
+            print("Playing...")
+            chanelUrl = channelSelected["cmd"].replace("ffmpeg ", "")
+            if "localhost" in chanelUrl:
+                chanelUrl = getLiveLink(mac, token, channelSelected["cmd"], host)
+            playWithMPV(chanelUrl, channelSelected["name"])
+
+            wait()
+            clear()
+
+
+def vodchoice(token=""):
+    if not token:
+        return []
+    vodgenres = getAllVodGenres(host, token, headers_g)
+
+    while True:
+        clear()
+        print("Vod: Genres")
+
+        for i, genre in enumerate(vodgenres):
+            print(f"{i+1}. {genre['title']}")
+
+        choixGenre = input("Choix du genre de vod: ")
+
+        if not choixGenre:
+            continue
+
+        if choixGenre.lower() == "q" or choixGenre.lower() == "0":
+            break
+
+        try:
+            choixGenre = int(choixGenre)
+        except Exception:
+            choixGenre = 1
+
+        genre = vodgenres[choixGenre - 1]
+
+        page = 1
+        while True:
+            clear()
+            print(f"{genre['title']} - Page: {page}")
+            if "id" not in genre or not genre["id"]:
+                break
+            vodByGenre = getAllVodbyGenres(
+                host, token, headers_g, "vod", genre["id"], str(page)
+            )
+
+            for i, vod in enumerate(vodByGenre):
+                print(f"{i+1}. {vod['name']}")
+
+            print(
+                """
+s: Suivant
+p: Precendent
+page x: to go to page x
+1-max element: to choose what vod to show
+            """
+            )
+
+            choice = input("Choix: ")
+
+            if choice.lower() == "s":
+                page = page + 1
+                continue
+            if choice.lower() == "p":
+                if page > 1:
+                    page = page - 1
+                continue
+            if "page " in choice:
+                try:
+                    page = int(choice.split(" ").pop())
+                except Exception:
+                    pass
+                continue
+            if choice.lower() == "q" or choice.lower() == "0":
+                break
+
+            try:
+                choice = int(choice)
+            except Exception:
+                choice = 1
+
+            vod = vodByGenre[choice - 1]
+
+            vod["url"] = getVodLink(token, vod["cmd"], host, "vod", headers_g)
+            if not vod["url"]:
+                print("Url invalide...Exiting")
+                continue
+            print(vod["name"])
+            print(vod["description"])
+
+            playWithMPV(vod["url"], vod["name"])
+
+            wait()
+
+        wait()
+
+
+def seriesChoice(token=""):
+    if not token:
+        return []
+    seriesgenres = getAllVodGenres(host, token, headers_g, type="series")
+
+    while True:
+        clear()
+        print("Series: Genres")
+
+        for i, genre in enumerate(seriesgenres):
+            print(f"{i+1}. {genre['title']}")
+
+        choixGenre = input("Choix du genre de la serie: ")
+
+        if not choixGenre:
+            continue
+
+        if choixGenre.lower() == "q" or choixGenre.lower() == "0":
+            break
+
+        try:
+            choixGenre = int(choixGenre)
+        except Exception:
+            choixGenre = 1
+
+        genre = seriesgenres[choixGenre - 1]
+
+        page = 1
+        while True:
+            clear()
+            print(f"{genre['title']} - Page: {page}")
+            if "id" not in genre or not genre["id"]:
+                break
+            vodByGenre = getAllVodbyGenres(
+                host, token, headers_g, "series", genre["id"], str(page)
+            )
+
+            for i, serie in enumerate(vodByGenre):
+                print(f"{i+1}. {serie['name']}")
+
+            print(
+                """
+s: Suivant
+p: Precendent
+page x: to go to page x
+1-max element: to choose what vod to show
+            """
+            )
+
+            choice = input("Choix: ")
+
+            if choice.lower() == "s":
+                page = page + 1
+                continue
+            if choice.lower() == "p":
+                if page > 1:
+                    page = page - 1
+                continue
+            if "page " in choice:
+                try:
+                    page = int(choice.split(" ").pop())
+                except Exception:
+                    continue
+
+            if choice.lower() == "q" or choice.lower() == "0":
+                break
+
+            try:
+                choice = int(choice)
+            except Exception:
+                choice = 1
+
+            serie = vodByGenre[choice - 1]
+
+            while True:
+                clear()
+                print(f"{serie['name']}")
+                print(f"{serie['description']}")
+                print(f"Has files: {serie['has_files']=='1'}")
+
+                seasons = getVodDetails(token, serie["id"], host, "series", headers_g)
+
+                if not seasons or not len(seasons):
+                    break
+
+                print("Seasons:")
+                for i, season in enumerate(seasons):
+                    print(f"{i+1}. {season['name']}")
+
+                choice = input("Choix: ")
+
+                choice = handleChoice(choice)
+
+                if choice == Consts.toBreak:
+                    break
+
+                selectedSeason = seasons[choice - 1]
+
+                if not (selectedSeason and len(selectedSeason["series"])):
+                    print("Pas d'episodes")
+                    wait()
+                    break
+
+                while True:
+                    clear()
+                    print("Episodes:")
+                    for i, episode in enumerate(selectedSeason["series"]):
+                        print(f"{i+1}. Episode{episode}")
+
+                    choice = input("Choix: ")
+
+                    choice = handleChoice(choice)
+
+                    if choice == Consts.toBreak:
+                        break
+
+                    url = getVodLink(
+                        token, selectedSeason["cmd"], host, "vod", headers_g, choice
+                    )
+
+                    playWithMPV(url, serie["name"])
+
+                    wait()
+
+            wait()
+
+
+while True:
+    clear()
+    token = getToken(host, headers_token)
+    if not token:
+        print("Token non généré\n")
+        continue
+    print(f"Token generé :{token}\r")
+
+    print(
+        """
+Choix:
+1. Afficher les canaux
+2. Afficher les VOD
+3. Afficher les series
+4. Quitter (q ou 0 aussi pour quitter)
+          """
+    )
+
+    choix = input("Votre choix : ")
+
+    if choix == "0" or choix.lower() == "q" or choix == "4":
+        break
+
+    if choix == "1":
+        tvChoice(token)
+    if choix == "2":
+        vodchoice(token)
+    if choix == "3":
+        seriesChoice(token)
---
+++ utils.py
@@ -0,0 +1,391 @@
+import json
+
+# import math
+import os
+
+# import random
+import requests
+
+# import mpv
+from enum import Enum
+import math
+
+headers_g = {
+    "User-Agent": "Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3",
+    "X-User-Agent": "Model: MAG250; Link: WiFi",
+    # "Referer": portal_url,
+    # "Cookie": f"mac={mac}; stb_lang=en; timezone=Europe/Amsterdam;",
+    # "Host": urlparse(portal_url).netloc,
+    "Accept": "*/*",
+}
+
+headers_token = {
+    "User-Agent": "Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3",
+    "X-User-Agent": "Model: MAG250; Link: WiFi",
+    "Accept": "*/*",
+}
+
+
+def getToken(server_host: str, headers: dict):
+    global proxies
+    global lastRefresh
+    params = "/server/load.php?type=stb&action=handshake&token=&JsHttpRequest=1-xml"
+    h_eader = requests.utils.default_headers()
+    h_eader.update(headers)
+
+    try:
+        res = None
+        try:
+            res = requests.get(
+                f"{server_host}{params}",
+                headers=h_eader,
+                timeout=15,
+            )
+
+        except (
+            # or requests.exceptions.ConnectTimeout
+            requests.exceptions.ReadTimeout
+            # or ReadTimeoutError
+        ) as e:
+            print(e)
+            return None
+        except requests.exceptions.ConnectionError as e:
+            print(e)
+            return None
+
+        resjson = {}
+
+        if res.status_code == 200:
+            if not res.content.decode():
+                return None
+            resjson = json.loads(res.content.decode())
+            try:
+                if "js" not in resjson:
+                    return None
+                assert resjson["js"]
+                assert resjson["js"]["token"]
+
+                return resjson["js"]["token"]
+
+            except AssertionError as e:
+                print(e)
+                print("[*] Failed Token")
+        elif res.status_code == 403:
+            print("BLOCKED")
+
+        return None
+
+    except TimeoutError or requests.exceptions.ConnectTimeout as e:
+        print(e)
+    except Exception as e:
+        print(e)
+
+    return None
+
+
+def getAllStreams(token: str, server_host: str, headers: dict):
+
+    if token:
+        params = "/server/load.php?type=itv&action=get_all_channels&JsHttpRequest=1-xml"
+
+        headers["Authorization"] = f"Bearer {token}"
+
+        res = None
+        try:
+
+            res = requests.get(
+                f"{server_host}{params}",
+                headers=headers,
+                timeout=20,
+            )
+
+        except TimeoutError or requests.exceptions.ConnectTimeout:
+            return None
+        except requests.exceptions.ConnectionError as e:
+            print(e)
+            return None
+        except requests.exceptions.ChunkedEncodingError as e:
+            print(e)
+            return None
+
+        if res.status_code >= 400:
+            print(f"[*]{res.reason} even with {token}")
+            return []
+
+        if len(res.content) < 100:
+            print(f"RES: {res.text}")
+            print("[x]Too less data to parse")
+            return []
+
+        # print(f"[*] OK: <{res.content.decode()[0:30]}>")
+
+        try:
+            assert res.content.decode() != ""
+            data = json.loads(res.content.decode())
+            assert data["js"]["data"]
+            channnels = data["js"]["data"]
+            return list(channnels)
+
+        except AssertionError:
+            return []
+        except TimeoutError or requests.exceptions.ConnectTimeout as e:
+            print(e)
+            return []
+        except json.decoder.JSONDecodeError as e:
+            print(e)
+            print(res.content.decode())
+            return []
+
+    else:
+        print("[*]ERROR: Token Invalid")
+
+
+def wait(label=""):
+    if not label:
+        label = "Entrer pour continuer"
+    return input(f"{label}...")
+
+
+def getAllGenres(host, headers, token):
+
+    if token:
+        params = "/server/load.php?type=itv&action=get_genres&JsHttpRequest=1-xml"
+
+        headers["Authorization"] = f"Bearer {token}"
+        res = requests.get(f"{host}{params}", headers=headers)
+        try:
+            assert res.content.decode() != ""
+            data = json.loads(res.content.decode())
+            assert data["js"]
+            genres = {}
+
+            for i in data["js"]:
+                genres[i["id"]] = i["title"]
+            return genres
+        except AssertionError:
+            return {}
+    else:
+        print("Error: Token Invalid")
+        return None
+
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def playWithMPV(url, title=""):
+    print(f"Playing: {title}")
+    if not url:
+        return
+    try:
+        # os.system(f"vlc --meta-title='{title}' '{url}' 2> /dev/null")
+        os.system(
+            "mpv --title=\"{title_}\" '{url}' 2> /dev/null".format(
+                title_=title.replace("(", "").replace(")", ""), url=url
+            )
+        )
+        # player = mpv.MPV(ytdl=True)
+        # player.play(url)
+        # player.wait_for_playback(timeout=10)
+    except Exception as e:
+        print(f"Error playing {title or url}: {str(e)}")
+        pass
+
+
+def getLiveLink(mac, token, cmd, host):
+    if not token:
+        return None
+    cookies = {"mac": mac, "stb_lang": "fr", "timezone": "GMT"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (QtEmbedded; U; Linux; C)",
+        "Authorization": "Bearer " + token,
+    }
+    try:
+        response = requests.get(
+            host
+            + "/server/load.php?type=itv&action=create_link&cmd="
+            + cmd
+            + "&series=0&forced_storage=false&disable_ad=false&download=false&force_ch_link_check=false&JsHttpRequest=1-xml",
+            cookies=cookies,
+            headers=headers,
+        )
+
+        data = response.json()
+        link_ = data["js"]["cmd"].split()[-1]
+        if link_:
+            return link_
+    except Exception as e:
+        print(e)
+        return None
+
+
+def getAllVod(host: str, token: str, headers: dict, type="vod"):
+
+    if type not in ["vod", "series"]:
+        return []
+
+    if not token:
+        return []
+
+    if token:
+        headers["Authorization"] = f"Bearer {token}"
+        page = 1
+        pages = 1
+        total_items = 1
+        max_page_items = 1
+
+        contents = []
+
+        while True:
+            print(f"Page: {page}/{pages}")
+            params = f"/server/load.php?type={type}&action=get_ordered_list&p={page}&sortby=added&JsHttpRequest=1-xml"
+
+            res = requests.get(f"{host}/{params}", headers=headers)
+            try:
+                assert res.content.decode() != ""
+                data = json.loads(res.content.decode())
+                # print(data)
+                assert data["js"]["data"]
+                total_items = data["js"]["total_items"]
+                max_page_items = data["js"]["max_page_items"]
+                pages = math.ceil(total_items / max_page_items)
+                pages = 10
+
+                contents.extend(data["js"]["data"])
+                page = page + 1
+                if page > pages:
+                    break
+            except AssertionError:
+                print("Nothing")
+                continue
+            except Exception:
+                print("Nothing")
+                continue
+
+        return contents
+    else:
+        return []
+
+
+def getAllVodGenres(host: str, token: str, headers: dict, type="vod"):
+
+    if type not in ["vod", "series"]:
+        return []
+
+    if not token:
+        return []
+
+    if token:
+        headers["Authorization"] = f"Bearer {token}"
+        params = f"/server/load.php?type={type}&action=get_categories"
+        res = requests.get(f"{host}/{params}", headers=headers)
+
+        try:
+            assert res.content.decode() != ""
+            data = json.loads(res.content.decode())
+            assert data["js"]
+            return data["js"]
+        except Exception:
+            print("Nothing")
+
+        return []
+    else:
+        return []
+
+
+def getAllVodbyGenres(
+    host: str, token: str, headers: dict, type="vod", categoryId="0", page="1"
+):
+
+    if type not in ["vod", "series"]:
+        return []
+
+    if not token:
+        return []
+
+    if token:
+        headers["Authorization"] = f"Bearer {token}"
+
+        params = f"/server/load.php?type={type}&action=get_ordered_list&category={categoryId}&genre=0&sortby=&p={page}"
+        res = requests.get(f"{host}/{params}", headers=headers)
+
+        try:
+            assert res.content.decode() != ""
+            data = json.loads(res.content.decode())
+            assert data["js"]
+            assert data["js"]["data"]
+            return data["js"]["data"]
+        except Exception:
+            print("Nothing")
+
+        return []
+    else:
+        return []
+
+
+def getVodLink(token, cmd, host, type="vod", headers={}, ep=""):
+    if not token:
+        return None
+    headers["User-Agent"] = (
+        "Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3"
+    )
+    headers["Authorization"] = f"Bearer {token}"
+    try:
+        api = f"{host}/server/load.php?type={type}&action=create_link&cmd={cmd}{f'&series={ep}' if ep !='' else ''}&force_ch_link_check=0"
+        response = requests.get(
+            api,
+            headers=headers,
+        )
+
+        if response.status_code >= 400:
+            return None
+
+        data = response.json()
+
+        return (
+            data["js"]["cmd"].split()[-1]
+            if "js" in data and "cmd" in data["js"]
+            else None
+        )
+    except Exception as e:
+        print(e)
+        return None
+
+
+def getVodDetails(token, movie_id, host, type="vod", headers={}):
+    if not token:
+        return None
+    headers["User-Agent"] = (
+        "Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3"
+    )
+    headers["Authorization"] = f"Bearer {token}"
+    try:
+        api = f"{host}/server/load.php?type={type}&action=get_ordered_list&movie_id={movie_id}&p=1"
+        response = requests.get(
+            api,
+            headers=headers,
+        )
+
+        if response.status_code >= 400:
+            return []
+
+        data = response.json()
+
+        return data["js"]["data"] if "js" in data and "data" in data["js"] else []
+    except Exception as e:
+        print(e)
+        return []
+
+
+class Consts(Enum):
+    toBreak = "BREAK"
+
+
+def handleChoice(choice):
+    if choice.lower() == "q" or choice.lower() == "0":
+        return Consts.toBreak
+    try:
+        choice = int(choice)
+    except Exception:
+        choice = 1
+    return choice
